fa9659ef52d5cfc96e7a1c013c5c344a24c09771
owen <owen.chosen@gmail.com>
2023-08-09 21:20:37 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+
+function submitData (username, email) {
+
+    fetch ('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: username,
+            email: email,
+        })
+    })
+    .then (response => response.json())
+    .then (objectData => {
+        document.body.innerHTML = `<h1>${objectData.id}</h1>`
+    })
+    .catch (error => error.message)
+}
+
+submitData ('Onyechefu Owen','owen.chosen@gmail.com');
+
+    
